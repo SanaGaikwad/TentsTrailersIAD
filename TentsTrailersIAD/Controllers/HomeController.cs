0e8bb442353f9b467a59d2ed4527034168253f4d
Sana Gaikwad <sgai0002@student.monash.edu>
2019-10-03 14:53:05 +1000

    Email Support

--- TentsTrailersIAD/Controllers/HomeController.cs
+++ TentsTrailersIAD/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TentsTrailersIAD;
+using TentsTrailersIAD.Models;
+using TentsTrailersIAD.Utils;
 
 namespace TentsTrailersIAD.Controllers
 {
@@ -16,16 +19,45 @@ namespace TentsTrailersIAD.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+          
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View(new SendEmailViewModel());
+        }
+             [HttpPost]
+        public ActionResult Contact(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents);
+
+                    ViewBag.Result = "Thank You! We will get back to you shortly.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
 
             return View();
-        }
+        
+
+
+    }
     }
 }
---
+++ TentsTrailersIAD/Utils/EmailSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace TentsTrailersIAD.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@localhost.com", "Tents&Trailers");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
